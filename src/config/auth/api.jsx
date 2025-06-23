9752fc54bde18c663a8baf00f0fc24479c786fb7
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-06-23 16:41:30 +0500

    fix: remove unnecessary whitespace in api configuration

--- src/config/auth/api.jsx
+++ src/config/auth/api.jsx
@@ -9,7 +9,7 @@ const api = axios.create({
   },
   withCredentials: true,
 });
-// 
+//
 export const getAccessToken = () => localStorage.getItem("accessToken");
 export const getRefreshToken = () => localStorage.getItem("refreshToken");
 
